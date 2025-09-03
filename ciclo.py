6c27a1c9c805631b8e1f5fe6c9552a0a2a94381e
Andres Mazariegos <andresmaza2309@gmail.com>
2025-09-03 12:40:02 -0600

    Hice los primeros ajustes
    Lector y escritura simple de archivos, meotodo para que el programa interprete lo suyo

---
+++ ciclo.py
@@ -0,0 +1,20 @@
+class palabrasReservadas():
+    def __init__(self):
+        pass
+
+    def cicloPalabras(self, palbra):
+        if(palbra == ""):
+            pass
+        elif(palbra == ""):
+            pass
+        elif(palbra == ""):
+            pass
+        elif(palbra == ""):
+            pass
+        elif(palbra == ""):
+            pass
+        elif(palbra == ""):
+            pass
+        elif(palbra == ""):
+            pass
+    
--- program.py
+++ program.py
@@ -1,1 +1,3 @@
-
+class Program:
+    def __init__(self):
+        pass
